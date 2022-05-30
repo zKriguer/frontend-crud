4a8943cbf87dd59cced9ea43661a756920eee42f
Kriguer <77925501+zKriguer@users.noreply.github.com>
2022-05-30 19:59:13 -0300

    reup entrar.js

---
+++ entrar.js
@@ -0,0 +1,23 @@
+let submit = document.getElementById('submit')
+
+let form = document.forms[0]
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+})
+
+submit.addEventListener('click', async () =>{
+    const regxEmail = /^[a-z0-9.]+@[a-z]+\.[a-z]{2,3}(\.[a-z]{2})?$/i
+    const regxPassword = /^.+/i
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    if(!regxEmail.test(email)) alert('Email indecente')
+    if(!regxPassword.test(password)) alert('Senha invalida')
+
+    const user = { email, password }
+
+    const get = await (await fetch(`https://backend-kriguer.herokuapp.com/api/users/${encodeURIComponent(email)}`)).json()
+    
+    if(password != get.password || email != get.email) {return alert('Dados Incorretos tente novamente')}
+    else{ window.location.href = `index.html?user=${email}`}
+})
